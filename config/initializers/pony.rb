08683131436a8680662228665fcecdc568be4860
Sean Rucker <sean@cadee.co>
2012-05-14 16:03:34 -0400

    Initial commit.

---
+++ config/initializers/pony.rb
@@ -0,0 +1,13 @@
+Pony.options = {
+  :to => 'srucker@gmail.com',
+  :via => :smtp,
+  :via_options => {
+    :address              => 'smtp.gmail.com',
+    :port                 => '587',
+    :enable_starttls_auto => true,
+    :user_name            => 'srucker',
+    :password             => 'Perrier27',
+    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+    :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+  }
+}
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContactForm::Application.config.secret_token = '6802c593e67c2a077e5817bb62a67ca8c08b2c8a5f2fc644086c4911dcf72fd8bc6e2bd8aa575906903f724be6fd775a56e408abfaf0923962d0b007598e1394'
